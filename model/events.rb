f803ab40ac6875be4260e3091261083acb654a05
Jackson N <44676403+thejayneo@users.noreply.github.com>
2020-10-01 18:06:44 +1000

    Altered text of random event to be more gender neutral.

--- model/events.rb
+++ model/events.rb
@@ -5,7 +5,7 @@ module Events
             "From the colourful stalls, it seems like the merchant is in town. Maybe the food at the inn will taste better tonight...",
             "A heavy fog hangs over town and the main street is eerily quiet...",
             "Rain pours endlessly from the sky above and villagers dash for cover",
-            "The sun peeks from behind the clouds, and a ray of light illuminates the face of a female adventurer you never met before..."
+            "The sun peeks from behind the clouds, and a ray of light illuminates the face of a stunning adventurer you never met before..."
         ]
         town_events_list[rand(0..4)]
     end
