839d35a64f13f722c60bc893cf81c11d4f3026d2
Jackson N <44676403+thejayneo@users.noreply.github.com>
2020-10-01 21:08:53 +1000

    Added bash script for execution.

---
+++ lotrd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo gem install bundler
+sudo gem install colorize
+sudo gem install tty-prompt
+sudo gem install terminal-table
+sudo gem install rspec
+sudo gem install artii
+
+ruby lotrd.rb
